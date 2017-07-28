847a2cb14b1cf31ccbf1927533f1ef8681089f87
GMW4994 <GMW4994@users.noreply.github.com>
2017-07-28 08:44:22 +0100

    Add files via upload

---
+++ Powershell/DSCLogFileCleardown.ps1
@@ -0,0 +1,62 @@
+﻿﻿<#
+ Script Title:     DSCLogFileCleardown.ps1
+       
+ Author:           Gary Wells  
+ 
+ Creation Date:    25th July 2017
+
+ Revision History:
+   
+   
+#>   
+    # 
+    # This script will locate all .MOF and .JSON files relating to DSC and remove all but the last 7 days
+    # This value can be changed by modifying the $RetentionTimeInDays parameter below
+    #
+$RetentionTimeInDays = 449
+    #
+    # The operator will be asked to input the name of the remote host to clear down
+    #
+$Computername = read-host -prompt 'Please enter the Hostname of the server that you wish to clear down'
+    #
+    # The script will search the path C:\Windows\System32\Configuration\ConfigurationStatus\ on the selected host for all files
+    # Logic will then determine how many files are older than the specified Retention Time above
+    #
+[array]$files = invoke-command -ComputerName $Computername -ScriptBlock `
+{
+    $RetentionTimeInDays = $args[0]
+    Get-ChildItem 'C:\windows\System32\Configuration\ConfigurationStatus' | Where {$_.Creationtime -lt (get-date).AddDays(-$RetentionTimeInDays)} 
+} -ArgumentList $RetentionTimeInDays
+    #
+    # The number of files that can de deleted will be returned
+    #
+$filecount = $files.Count
+    # 
+    # If there are files to delete, a message is displayed with the number of eligible files that can be deleted
+    #
+if ($filecount -ge 1) {
+        #
+        # The operator must then confirm he or she wishes to proceed and cleanse the folder
+        # Only YES will result in the deletions being processed. Any other key / phrase will exit the script. 
+        # This value is NOT case-sensitive as all input values will be forced to Upper Case
+        #   
+    $response = read-host -Prompt "$ComputerName contains $filecount file(s) older than $RetentionTimeInDays days. Would you like to delete them? (Type YES to confirm)"
+    if(($response.ToUpper()) -ne "YES"){EXIT}
+        #
+        # The script will then clear all of the eligible files and return a confirmation
+        #     
+    $filedelete = invoke-command -ComputerName $Computername -ScriptBlock `
+    {
+        $RetentionTimeInDays = $args[0]
+        Get-ChildItem 'C:\windows\System32\Configuration\ConfigurationStatus' | Where {$_.Creationtime -lt (get-date).AddDays(-$RetentionTimeInDays)} | Remove-Item -Force
+    } -ArgumentList $RetentionTimeInDays
+
+    Write-Host "Deleted $filecount files from $Computername"
+}
+else {
+    #
+    # If there are NO eligible files to delete, a message is displayed stating there are zero files to delete and the script will exit
+    #
+    Write-Host "There are zero files to delete from $Computername"
+}
+# (get-date).AddDays(-450)
---
+++ Powershell/EventLogCleardown.ps1
@@ -0,0 +1,108 @@
+﻿﻿<#
+ Script Title:     EventLogCleardown.ps1
+       
+ Author:           Gary Wells  
+ 
+ Creation Date:    25th July 2017
+
+ Version:          2
+
+ Revision History:
+ 27th July 20017   Gary Wells
+ Added the command to limit event log max size after the clear down command for each log file to prevent log expanding before the new MOF can be processed
+
+ 28th July 20017   Gary Wells
+ Added gpupdate /Target:Computer /force to enforce the policy settings immediately - by default there could be a 90 minute refresh window where the values could
+ be manually changed in the GUI
+
+   
+#>   
+    # 
+    # This script will clear down selected Event Logs on a target host
+    # Only APPLICATION, SYSTEM & SECURITY Event Logs can be targeted
+    # Log Files will be tested against a Maximum size value of ~1000MB
+    # Log files exceeding this size can be cleared down
+    #
+$MaxLogFileSizeInBytes = 1MB
+    #
+    # The operator will be asked to input the name of the remote host to clear down
+    #
+$Computername = read-host -prompt 'Please enter the Hostname of the server that you wish to clear down'
+    #
+    # The script will search the remote host for the 3 applicable Event Logs
+    #
+$LogDetails = invoke-command -ComputerName $Computername -ScriptBlock `
+{
+    Get-WmiObject -Class win32_nteventlogfile | Where-Object {$_.name -match "Security.evt?" -or $_.name -match "System.evt?" -or $_.name -match "Application.evt?"} 
+}
+    #
+    # The Event Log details will be displayed to the operator
+    #
+Write-Host "The Event Log details for $Computername are" 
+$LogDetails | select Filesize,logfilename,name,numberofrecords | ft | Out-String | %{Write-Host $_}
+    #
+$SecurityLogDetails = $LogDetails | where{$_.LogFileName -eq "Security"}
+$SystemLogDetails = $LogDetails | where{$_.LogFileName -eq "System"}
+$ApplicationLogDetails = $LogDetails | where{$_.LogFileName -eq "Application"}
+    #
+    # Logic will then determine if any of the logs need to to be cleared down (Those of 1GB or more)
+    # Eligible files will be presented to the operator with an option to clear them down
+    #
+    # This process will be performed individually on each of the 3 target log files
+    #
+    ################
+    # Security Log #
+    ################
+    #
+if ($SecurityLogDetails.FileSize -ge $MaxLogFileSizeInBytes)
+        {
+        Write-Host "The $Computername Security Log File can be cleared down"
+        $response = Read-Host -Prompt "Would you like to clear down this Log File? (Type YES to confirm)"
+            if(($response.ToUpper()) -eq "YES"){
+                Clear-EventLog -ComputerName $Computername -LogName Security -Verbose 
+                Limit-EventLog -LogName Security -ComputerName $Computername -OverflowAction OverwriteAsNeeded -MaximumSize 1GB 
+                Invoke-Command -ComputerName $Computername {gpupdate /Target:Computer /force}  
+        }
+    }
+else
+    {
+         Write-Host "The Security Log file is currently below the Maximum Size allowed. No action is required at this time"
+    }
+    #
+    ##############
+    # System Log #
+    ############## 
+    #
+if ($SystemLogDetails.FileSize -ge $MaxLogFileSizeInBytes)
+        {
+        Write-Host "The $Computername System Log File can be cleared down"
+        $response = Read-Host -Prompt "Would you like to clear down this Log File? (Type YES to confirm)"
+        if(($response.ToUpper()) -eq "YES"){
+                Clear-EventLog -ComputerName $Computername -LogName System -Verbose
+                Limit-EventLog -LogName System -ComputerName $Computername -OverflowAction OverwriteAsNeeded -MaximumSize 1GB
+                Invoke-Command -ComputerName $Computername {gpupdate /Target:Computer /force}
+        }
+    }
+else
+    {
+        Write-Host "The System Log file is currently below the Maximum Size allowed. No action is required at this time"
+    }
+    #
+    ###################
+    # Application Log #
+    ###################
+    #
+if ($ApplicationLogDetails.FileSize -ge $MaxLogFileSizeInBytes)
+        {
+        Write-Host "The $Computername Application Log File can be cleared down"
+        $response = Read-Host -Prompt "Would you like to clear down this Log File? (Type YES to confirm)"
+        if(($response.ToUpper()) -eq "YES"){
+               Clear-EventLog -ComputerName $Computername -LogName Application -Verbose
+               Limit-EventLog -LogName Application -ComputerName $Computername -OverflowAction OverwriteAsNeeded -MaximumSize 1GB
+               Invoke-Command -ComputerName $Computername {gpupdate /Target:Computer /force}
+        }
+    }
+else
+    {
+        Write-Host "The Application Log file is currently below the Maximum Size allowed. No action is required at this time"
+    }
